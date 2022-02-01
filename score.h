18071561dc54f48a65f18a9fc0b8147b311381c1
sano_yuya <k021g1256@g.neec.ac.jp>
2022-02-01 12:21:28 +0900

    スコア(仮)

--- score.h
+++ score.h
@@ -9,9 +9,13 @@ public:
 	void RC();//rank
 	void TC(int sceneflag);//time
 	void Setnohitflag(int nohitflag);
-	void Draw(int randX,int randY);
+	void Draw(int randX, int randY);
 	void Setscoreitem(int scoreitem);
 	void KnockDown();
+	void SetHp(int hp);
+	void Death(int& sceneflag);
+	void Clear(int& sceneflag);
+	void ResultDraw();
 
 private:
 	int score;
@@ -24,5 +28,12 @@ private:
 	int div;
 	int index;
 	int scoreitem;
+	int hp;
+	int knock;
+	int item;
+	int time;
+	int nodama;
+	int clear;
+	int hps;
 };
 
